233668891266ef05d0438337f23d4d2025dc347d
zuza <zuzannadamszel@gmail.com>
2021-12-30 12:02:50 +0100

    Corrections

--- assets/constans.py
+++ assets/constans.py
@@ -5,14 +5,13 @@ BOARD_BACKGROUND = 'images/mediumwood.jpg'
 ICON = 'images/icon3.png'
 FILE = 'images/GoQuickRef.pdf'
 
+FONT = '/usr/share/fonts/truetype/msttcorefonts/Arial.ttf'
 
 #font's properties - start screen
 START_TITLE_FONT_SIZE = 30
 START_OPTIONS_FONT_SIZE = 24
 
-
 #font's properties - main game screen
-FONT = '/usr/share/fonts/truetype/msttcorefonts/Georgia.ttf'
 FONT_SIZE = 18
 
 # colors in rgb 
